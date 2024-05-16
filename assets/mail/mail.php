685327b901dbe59771ae8661ce3c392dbd2b698e
Ghosthets <131135117+ghosthets@users.noreply.github.com>
2024-05-16 23:19:00 +0530

    Update mail.php

--- assets/mail/mail.php
+++ assets/mail/mail.php
@@ -15,7 +15,7 @@ Message: ".$message."
 
 ";
 
-mail ("example@gmail.com" , "New Message", $email_message);
+mail ("pygyxike@molecule.ink" , "New Message", $email_message);
 header("location: ../../mail-success.html");
 ?>
 
